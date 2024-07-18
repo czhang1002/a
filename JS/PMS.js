dc64a26f30ce7b16cda3f7a7d10c724cbbccf017
czhang1002 <107442740+czhang1002@users.noreply.github.com>
2024-07-18 15:53:36 -0400

    Update PMS.js

--- JS/PMS.js
+++ JS/PMS.js
@@ -1,3 +1,4 @@
+// navigation selection
 const navs = document.querySelectorAll("menu li");
 navs.forEach(nav => {
     nav.addEventListener("click", () => {
@@ -7,6 +8,20 @@ navs.forEach(nav => {
     function cleared_Selection() {
         navs.forEach(nav => {
             nav.classList.remove("active");
+        })
+    }
+})
+
+// pet record header sort selection (sort by selected field)
+const pet_record_header = document.querySelectorAll(".pet_record_header b");
+pet_record_header.forEach(selection => {
+    selection.addEventListener("click", () => {
+        cleared_Selection();
+        selection.classList.add("active");
+    })
+    function cleared_Selection() {
+        pet_record_header.forEach(selection => {
+            selection.classList.remove("active");
         })
     }
 })
